91d02d57354bf6576e12accc6eefd443bdab445a
Chris Darby <chrisdarby@gmail.com>
2013-12-18 15:00:06 +0000

    Create virtualhost-maker.sh

---
+++ virtualhost-maker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SERVERNAME=$1
+USER=$2
+OLDSTR=#serverName#
+OLDHOME=#homeDir#
+
+
+echo "Creating profile for $SERVERNAME"
+cp /etc/apache2/sites-available/template /etc/apache2/sites-enabled/$SERVERNAME
+sed -i "s/$OLDSTR/$SERVERNAME/g" /etc/apache2/sites-enabled/$SERVERNAME
+sed -i "s/$OLDHOME/$USER/g" /etc/apache2/sites-enabled/$SERVERNAME
+
+
+adduser $USER
+mkdir -p /home/$USER/public_html
+
+sudo chown -R $USER:$USER /home/$USER/public_html
+sudo chmod 755 /home/$USER/public_html
+
+echo "$SERVERNAME" >  /home/$USER/public_html/index.html
+
+service apache2 restart
+echo "Complete!"
