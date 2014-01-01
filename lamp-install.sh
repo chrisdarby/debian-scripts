df117f04525cc8382a96813f8def7dfce3485783
Chris Darby <chrisdarby@gmail.com>
2014-01-01 21:43:13 +0000

    Update lamp-install.sh
    PureFTPD install failed because on incorrect package name

--- lamp-install.sh
+++ lamp-install.sh
@@ -10,6 +10,6 @@ sudo apt-get -y update
 sudo apt-get -y install mariadb-server
 sudo apt-get -y install apache2
 sudo apt-get -y install php5 libapache2-mod-php5
-sudo apt-get -y install pureftp
+sudo apt-get -y install pureftpd
 sudo apt-get -y install php5-cli php5-common php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl php5-mysqlnd
 sudo apt-get -y install phpmyadmin
