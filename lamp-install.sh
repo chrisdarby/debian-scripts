2f32ae88b8673e04203ee03175e671caea3729c1
Chris Darby <chrisdarby@gmail.com>
2014-12-10 13:54:46 +0000

    Update lamp-install.sh

--- lamp-install.sh
+++ lamp-install.sh
@@ -6,9 +6,9 @@ sudo apt-get -y install php5 libapache2-mod-php5
 sudo apt-get -y install php5-cli php5-common php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl
 sudo apt-get -y install pure-ftpd
 sudo apt-get -y install phpmyadmin
-sudo apt-get -y sendmail-bin
-sudo apt-get -y sendmail
-sudo apt-get -y git
+sudo apt-get -y install sendmail-bin
+sudo apt-get -y install sendmail
+sudo apt-get -y install git
 
 wget https://raw.githubusercontent.com/salexis/debian-scripts/master/apache-template
 wget https://raw.githubusercontent.com/salexis/debian-scripts/master/virtualhost-maker.sh
