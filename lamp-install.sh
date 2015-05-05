c246f9efb44ff2fd8af2e6c8b9ba91de5da7e5c7
Chris Darby <chrisdarby@gmail.com>
2015-05-05 17:24:35 +0100

    Update lamp-install.sh
    Removed sudo for Debian 8

--- lamp-install.sh
+++ lamp-install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y install mysql-server
-sudo apt-get -y install apache2
-sudo apt-get -y install php5 libapache2-mod-php5
-sudo apt-get -y install php5-cli php5-common php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl
-sudo apt-get -y install pure-ftpd
-sudo apt-get -y install phpmyadmin
-sudo apt-get -y install sendmail-bin
-sudo apt-get -y install sendmail
-sudo apt-get -y install git
+apt-get -y update
+apt-get -y install mysql-server
+apt-get -y install apache2
+apt-get -y install php5 libapache2-mod-php5
+apt-get -y install php5-cli php5-common php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl
+apt-get -y install pure-ftpd
+apt-get -y install phpmyadmin
+apt-get -y install sendmail-bin
+apt-get -y install sendmail
+apt-get -y install git
 
 wget https://raw.githubusercontent.com/salexis/debian-scripts/master/apache-template
 wget https://raw.githubusercontent.com/salexis/debian-scripts/master/virtualhost-maker.sh
