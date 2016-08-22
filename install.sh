80a32653b2550fa0f9efe02be596de615076ccbf
Chris Darby <chrisdarby@gmail.com>
2016-08-22 13:57:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt-get -y install mysql-server
 apt-get -y install apache2
 apt-get -y install curl
 apt-get -y install php5 libapache2-mod-php5
-apt-get -y install php5-cli php5-common php5-imagick php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl pdo-mysql
+apt-get -y install php5-cli php5-common php5-imagick php5-curl php5-gd php5-imap php5-mcrypt php5-mysql php5-xmlrpc php5-intl
 apt-get -y install pure-ftpd
 apt-get -y install phpmyadmin
 apt-get -y install unzip
