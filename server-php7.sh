a6d6bf3b76bfdc004a153531cee29705929cb212
Chris Darby <chrisdarby@gmail.com>
2018-11-28 21:32:31 +0000

    Update server-php7.sh

--- server-php7.sh
+++ server-php7.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+apt-get remove unscd
 apt-get -y update
 apt-get -y install mysql-server
 apt-get -y install apache2
 apt-get -y install curl
+
 apt-get -y install apt-transport-https lsb-release ca-certificates
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+
 apt-get -y update
 apt-get -y install unzip
 apt-get -y install git
+
 apt-get -y install php7.2
 apt-get -y install php7.2-cli
 apt-get -y install libapache2-mod-php7.2
@@ -20,12 +24,17 @@ apt-get -y install php7.2-mysql
 apt-get -y install php7.2-xml
 apt-get -y install php7.2-zip
 apt-get -y install php7.2-json
+apt-get -y install php7.2-imap
+
+apt-get install phpmyadmin
 
 wget https://raw.githubusercontent.com/chrisdarby/debian-scripts/master/apache-template
+wget https://raw.githubusercontent.com/chrisdarby/debian-scripts/master/apache-template-ssl
 wget https://raw.githubusercontent.com/chrisdarby/debian-scripts/master/host.sh
 
 cp apache-template /etc/apache2/sites-available/template-standard
-cp host.sh /usr/bin/makehost
+cp apache-template-ssl /etc/apache2/sites-available/template-standard-ssl
+cp host.sh /usr/bin/makesite
 
 a2enmod rewrite
 service apache2 restart
