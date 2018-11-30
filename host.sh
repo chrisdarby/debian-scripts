a4fc1c1068251f255d412fea6b5a6cb5b70e8f68
Chris Darby <chrisdarby@gmail.com>
2018-11-30 10:32:08 +0000

    Update host.sh

--- host.sh
+++ host.sh
@@ -6,9 +6,9 @@ OLDHOME=#homeDir#
 
 
 echo "Creating profile for $SERVERNAME"
-cp /etc/apache2/sites-available/template-standard /etc/apache2/sites-available/$SERVERNAME.conf
-sed -i "s/$OLDSTR/$SERVERNAME/g" /etc/apache2/sites-available/$SERVERNAME.conf
-sed -i "s/$OLDHOME/$USER/g" /etc/apache2/sites-available/$SERVERNAME.conf
+cp /etc/apache2/sites-available/template-standard /etc/apache2/sites-available/$USER.conf
+sed -i "s/$OLDSTR/$SERVERNAME/g" /etc/apache2/sites-available/$USER.conf
+sed -i "s/$OLDHOME/$USER/g" /etc/apache2/sites-available/$USER.conf
 
 adduser $USER
 mkdir -p /home/$USER/public_html
@@ -18,7 +18,7 @@ chmod 755 /home/$USER/public_html
 
 echo "$SERVERNAME" >  /home/$USER/public_html/index.html
 
-a2ensite /etc/apache2/sites-available/$SERVERNAME.conf
+a2ensite /etc/apache2/sites-available/$USER.conf
 
 service apache2 restart
 echo "Complete!"
